2c35a6531a74996c1eee4f2a7f4cc1f332964f5f
atom-b <abreece@gmail.com>
2017-12-16 23:18:59 -0800

    day 17 - part 2

--- aoc17/Program.fs
+++ aoc17/Program.fs
@@ -1,28 +1,38 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
 open System.Collections.Generic
 
-[<EntryPoint>]
-let main argv =
-
+let part1 input iterations = 
     let buffer = new List<int>()
     buffer.Add(0)
     let mutable position = 0
 
-    let input = 301
-
-    for value = 1 to 2017 do
+    for value = 1 to iterations do
         position <- ((position + input) % buffer.Count)
-        //buffer.Insert((position + 1) % (buffer.Count - 1), value)
         buffer.Insert(position + 1, value)
         position <- position + 1
-        //printfn "position: %i" position
+        
+    buffer.Item((position + 1) % buffer.Count)
 
-    //buffer |> Seq.iter (fun d -> printf "%i," d)
+let part2 input iterations = 
+    let mutable position = 0
+    let mutable count = 1
+    let mutable afterZero = 0
 
-    printfn "solution 1: %i" <| buffer.Item(position + 1)
+    for value = 1 to iterations do
+        position <- ((position + input) % count)
+        if position = 0 then afterZero <- value
+        position <- position + 1
+        count <- count + 1
 
-    Console.ReadLine()
+    afterZero
 
-    0 // return an integer exit code
+[<EntryPoint>]
+let main argv =
+
+    let input = 301
+    printfn "solution 1: %i" <| part1 input 2017
+    printfn "solution 2: %i" <| part2 input 50000000
+
+    Console.ReadLine() |> ignore
+
+    0
