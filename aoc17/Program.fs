3c5cc45ab182d83c6cfedd2c6cf9feff6387a591
atom-b <abreece@gmail.com>
2017-12-17 16:51:00 -0800

    day 17 - part 2 with tail recursion

--- aoc17/Program.fs
+++ aoc17/Program.fs
@@ -1,5 +1,6 @@
 ﻿open System
 open System.Collections.Generic
+open Microsoft.FSharp.Core.Operators.OperatorIntrinsics
 
 let part1 input iterations = 
     let buffer = new List<int>()
@@ -26,13 +27,21 @@ let part2 input iterations =
 
     afterZero
 
+let rec part2Recursive input (iteration, remaining, pos, az) = 
+    match remaining with
+    | 0 -> az
+    | _ ->
+        let position = ((pos + input) % iteration)
+        let afterZero = (if position = 0 then iteration else az)
+        part2Recursive input ((iteration + 1), (remaining - 1), position + 1, afterZero)
+
 [<EntryPoint>]
 let main argv =
 
     let input = 301
     printfn "solution 1: %i" <| part1 input 2017
     printfn "solution 2: %i" <| part2 input 50000000
-
+    printfn "solution 2_proper: %i" <| part2Recursive input (1, 50000000, 0, 0)
     Console.ReadLine() |> ignore
 
     0
