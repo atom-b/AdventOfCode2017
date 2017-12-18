98444d5bd7f32022e305f94159f20e38d2137c5f
atom-b <abreece@gmail.com>
2017-12-17 17:40:48 -0800

    day 17 - immutable part 1 with tail recursion

--- aoc17/Program.fs
+++ aoc17/Program.fs
@@ -1,6 +1,5 @@
 ﻿open System
 open System.Collections.Generic
-open Microsoft.FSharp.Core.Operators.OperatorIntrinsics
 
 let part1 input iterations = 
     let buffer = new List<int>()
@@ -13,6 +12,22 @@ let part1 input iterations =
         position <- position + 1
         
     buffer.Item((position + 1) % buffer.Count)
+
+let rec part1Recursive input (iteration, remaining, position, buffer:List<int>) =
+    match remaining with
+    | 0 -> buffer.Item((position + 1) % buffer.Count)
+    | _ ->
+        let pos = ((position + input) % buffer.Count) + 1
+        buffer.Insert(pos, iteration)
+        part1Recursive input (iteration + 1, remaining - 1, pos, buffer)
+
+let rec part1Immutable input (iteration, remaining, position, buffer:list<int>) = 
+    match remaining with 
+    | 0 -> buffer.[((position + 1) % buffer.Length)]
+    | _ ->
+            let pos = ((position + input) % buffer.Length)
+            let buf = buffer.[0..pos] @ iteration :: if pos = buffer.Length - 1 then [] else buffer.[pos+1..]
+            part1Immutable input (iteration + 1, remaining - 1, pos + 1, buf)
 
 let part2 input iterations = 
     let mutable position = 0
@@ -27,21 +42,27 @@ let part2 input iterations =
 
     afterZero
 
-let rec part2Recursive input (iteration, remaining, pos, az) = 
+let rec part2Recursive input (iteration, remaining, position, afterZero) = 
     match remaining with
-    | 0 -> az
+    | 0 -> afterZero
     | _ ->
-        let position = ((pos + input) % iteration)
-        let afterZero = (if position = 0 then iteration else az)
-        part2Recursive input ((iteration + 1), (remaining - 1), position + 1, afterZero)
+        let pos = ((position + input) % iteration)
+        let az = (if pos = 0 then iteration else afterZero)
+        part2Recursive input ((iteration + 1), (remaining - 1), pos + 1, az)
 
 [<EntryPoint>]
 let main argv =
 
     let input = 301
     printfn "solution 1: %i" <| part1 input 2017
+
+    let buffer = new List<int>()
+    buffer.Add(0)
+    printfn "solution 1 recursive: %i" <| part1Recursive input (1, 2017, 0, buffer)
+    printfn "solution 1 immutable: %i" <| part1Immutable input (1, 2017, 0, [0;])
+
     printfn "solution 2: %i" <| part2 input 50000000
-    printfn "solution 2_proper: %i" <| part2Recursive input (1, 50000000, 0, 0)
+    printfn "solution 2 recursive: %i" <| part2Recursive input (1, 50000000, 0, 0)
     Console.ReadLine() |> ignore
 
     0
