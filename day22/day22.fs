78cf3ec0ff46ac5c6c0c76d68e3fc2f9392a9db5
atom-b <abreece@gmail.com>
2017-12-21 23:08:30 -0800

    day 22 - part 1

---
+++ day22/day22.fs
@@ -0,0 +1,95 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.Numerics
+
+let infectedState = "#"
+let cleanState = "."
+
+let readLines filePath = System.IO.File.ReadLines(filePath)
+
+let inputToGrid (input:string[]) =
+        input |> Array.map (fun row -> row.ToCharArray() |> Array.map (fun c -> c.ToString()) |> Array.map (fun s -> if s = infectedState then true else false))
+
+let gridToDict (g:bool[][]) = 
+
+    // not proud of this...
+    let mutable gdict = Map.ofList[]
+    for y in 0 .. g.Length - 1 do
+        let row = g.[y]
+        for x in 0 .. (row.Length - 1) do
+            gdict <- gdict.Add((x,y),g.[y].[x])
+
+    gdict
+
+let right dir = 
+    let dir_x, dir_y = dir
+    if dir_x = 1 then (0, -1)
+    else if dir_x = -1 then (0,1)
+    else if dir_y = 1 then (1,0)
+    else (-1,0)
+
+let left dir = 
+    let dir_x, dir_y = dir
+    if dir_x = 1 then (0, 1)
+    else if dir_x = -1 then (0,-1)
+    else if dir_y = 1 then (-1,0)
+    else (1,0)
+
+let printGrid (grid:bool[][]) = 
+
+    for i in (grid.Length - 1) .. -1 .. 0 do  
+       grid.[i] |> Array.iter (fun cell -> 
+                let c = if cell then "#" else "."
+                printf "%s " c)
+       printfn ""
+        
+
+let rec step steps infections (carrier:((int*int)*(int*int))) (grid:Map<(int32*int32),bool>) =
+    let pos,dir = carrier
+    let pos_x,pos_y = pos
+    let mutable newInfection = 0
+
+    if steps <= 0 then
+        infections
+    else
+
+    let infected = if grid.ContainsKey(pos) then grid.[(pos)] else false
+    let newGrid = grid.Add(pos, not infected)
+    let new_dir = if infected then right dir else left dir
+
+    if infected = false then newInfection <- 1
+            
+    let new_x,new_y = new_dir
+    let newPos = (pos_x + new_x , pos_y + (new_y))
+
+    step (steps - 1) (infections + newInfection) (newPos,new_dir)  newGrid
+
+[<EntryPoint>]
+let main argv =
+
+    //let testInput = readLines "testinput.txt" |> Seq.toArray
+    //Array.Reverse(testInput)
+    let input = readLines "input.txt" |> Seq.toArray
+    Array.Reverse(input)
+    let iterations =  10000
+
+    let grid = inputToGrid input
+
+    printGrid grid
+
+    let width = grid.[0].Length + (grid.[0].Length % 2) - 1
+    let height = grid.Length + (grid.Length % 2) - 1
+
+    let start = (width / 2, height / 2)
+    let direction = (0,1)
+    
+    let dict = gridToDict grid
+
+    let bursts = step iterations 0 (start,direction) dict
+
+    printfn "infections: %i" bursts
+
+    Console.ReadLine() |> ignore
+    
+    0 // return an integer exit code
