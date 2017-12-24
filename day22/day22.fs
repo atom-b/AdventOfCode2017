5a5bf14f4d6b442d15563cf52ed815c9efc18a78
Adam <atom-b@users.noreply.github.com>
2017-12-23 22:25:24 -0800

    day 22 - part 1 cleanup, a little part 2

--- day22/day22.fs
+++ day22/day22.fs
@@ -32,23 +32,37 @@ let left dir =
     else if dir_y = 1 then (-1,0)
     else (1,0)
 
-let updateNodePart1 (node:((int*int)*string)) =
+let reverse dir =
+    dir |> left |> left
+
+let nextState1 (node:((int*int)*string)) =
     let pos,state = node
     let newState =
         match state with
         | s when s = cleanState -> infectedState
         | s when s = infectedState -> cleanState
+        | _ -> cleanState
     (pos,newState)
 
-// let updateNodePart2 (node:((int*int)*string)) =
-//     let pos,state = node
-//     let newState =
-//         match state with
-//         | cleanState -> weakenedState
-//         | weakenedState -> infectedState
-//         | infectedState -> flaggedState
-//         | flaggedState -> cleanState
-//     (pos,newState)
+let nextState2 (node:((int*int)*string)) =
+    let pos,state = node
+    let newState =
+        match state with
+        | s when s =  cleanState -> weakenedState
+        | s when s =  weakenedState -> infectedState
+        | s when s =  infectedState -> flaggedState
+        | s when s =  flaggedState -> cleanState
+    (pos,newState)
+
+let turn (node:((int*int)*string)) dir =
+    let _,state = node
+
+    match state with
+    | s when s = cleanState -> left dir
+    | s when s = weakenedState -> dir
+    | s when s = infectedState -> right dir
+    | s when s = flaggedState -> reverse dir
+    | _ -> dir
 
 let getNode (grid:Map<(int32*int32),string>) (pos:(int*int)) =
     match grid.TryFind pos with
@@ -67,27 +81,28 @@ let printGrid (grid:string[][]) =
                 printf "%s " c)
        printfn ""
 
-let rec step steps infections (carrier:((int*int)*(int*int))) (grid:Map<(int32*int32),string>) =
-    let pos,dir = carrier
-    let pos_x,pos_y = pos
+let rec stepPart1 steps infections (carrier:((int*int)*(int*int))) (grid:Map<(int32*int32),string>) =
 
     if steps <= 0 then
         infections
     else
 
+    let pos,dir = carrier
+    let pos_x,pos_y = pos
+    let node = getNode grid pos
+
+    let newGrid = grid.Add <| nextState1 node
+
     let newInfection =
-        if getNode grid pos |> getState = infectedState
+        if node |> getState = infectedState
         then 0
         else 1
 
-    let newDir = if newInfection = 0 then right dir else left dir
-    let updatedNode = updateNodePart1 (pos, if grid.ContainsKey(pos) then grid.[(pos)] else cleanState)
-    let newGrid = grid.Add(updatedNode)
-
+    let newDir = turn node dir
     let newX,newY = newDir
-    let newPos = (pos_x + newX , pos_y + (newY))
+    let newPos = (pos_x + newX, pos_y + newY)
 
-    step (steps - 1) (infections + newInfection) (newPos,newDir)  newGrid
+    stepPart1 (steps - 1) (infections + newInfection) (newPos,newDir)  newGrid
 
 [<EntryPoint>]
 let main argv =
@@ -107,12 +122,19 @@ let main argv =
 
     let start = (width / 2, height / 2)
     let direction = (0,1)
-
     let dict = gridToDict grid
 
-    let bursts = step iterations 0 (start,direction) dict
+    let infectionsP1 = stepPart1 iterations 0 (start,direction) dict
 
-    printfn "infections: %i" bursts
+    printfn "infections: %i" infectionsP1
+
+    assert (infectionsP1 = 5575)
+
+    let iterationsP2 = 10000000
+    // let infectionsP2 = stepPart2 iterations 0 (start,direction) dict
+    // printfn "infections 2: %i" infectionsP2
+
+    printfn "%A" DateTime.Now
 
     // Console.ReadLine() |> ignore
 
